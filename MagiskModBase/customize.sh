bb2799ce464409f707d9d5e7b14b5bb3a920b1c3
ncsufan8628 <510c8806@opayq.com>
2025-09-02 07:58:50 -0400

    Update customize.sh
    remove pixel requirement

--- MagiskModBase/customize.sh
+++ MagiskModBase/customize.sh
@@ -115,19 +115,19 @@ testKernelSU()
     	fi;
     fi;
 }
-
-assertPixelRom()
-{
-	PixelTipsPattern="TipsPrebuilt*"
-	PixelTipsParent="/product/priv-app"
-
-  if ! find "$PixelTipsParent" -maxdepth 1 -name "$PixelTipsPattern" -print -quit | grep -q .; then
-  	ui_print 'Device does not seem to be a Pixel'
-  	ui_print 'phone, containing an original ROM.'
-
-    abort 'Installation aborted due to incompatibility'
-  fi
-}
+#
+#assertPixelRom()
+#{
+#	PixelTipsPattern="TipsPrebuilt*"
+#	PixelTipsParent="/product/priv-app"
+#
+#  if ! find "$PixelTipsParent" -maxdepth 1 -name "$PixelTipsPattern" -print -quit | grep -q .; then
+#  	ui_print 'Device does not seem to be a Pixel'
+#  	ui_print 'phone, containing an original ROM.'
+#
+#    abort 'Installation aborted due to incompatibility'
+ # fi
+#}
 
 assert16QPR()
 {
